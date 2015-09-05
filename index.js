6d35f6d810f06201327a9fa373acf90b90128c8a
Mike Dvorscak <mike.dvorscak@gmail.com>
2015-09-05 17:38:28 -0500

    Basic args validation done
    args function can't handle optional parameters or nested parameters yet.
    
    Added some missing false positive test cases.
    
    Extracted custom validations to their own file to make the code less
    tightly coupled.

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 /**
  * Created by Mike Dvorscak on 9/2/2015.
  */
-var _ = require('underscore');
+var typeChecker = require('./lib/custom-type-checker');
+/** @constant {number} */
 var LOW_VERBOSITY = 0;
+/** @constant {number} */
 var HIGH_VERBOSITY = 1;
 
+/**
+ *
+ * @param [opts={}] {object}
+ * @param [opts.verbosityLevel={@link LOW_VERBOSITY}] {number} - if set to {@link HIGH_VERBOSITY} validation failures
+ * will throw errors instead of returning false
+ * @param [opts.errorClass=Error] {class} - custom error class to use instead of built-in Error
+ * @constructor
+ */
 function Validator(opts){
     opts = opts || {};
     this.verbosityLevel = opts.verbosityLevel || LOW_VERBOSITY;
     this.ErrorClass = opts.errorClass || Error;
 }
 
-//
-var specialValidations = {
-    'date': _.isDate,
-    'array': _.isArray
-};
-
+/**
+ *
+ * @param item {object|array|string|number|date} - item to check the type of
+ * @param expectedType - the expected type of the item
+ * @param [argName=''] - an alias for item, used to customize the error if the validation fails
+ * @throws - throws an instance of {@link Validator.ErrorClass} if the validation fails in high verbosity mode
+ * @returns {boolean} - true if the item is the correct type, false otherwise.
+ */
 Validator.prototype.arg = function arg(item, expectedType, argName){
     var isCorrectType;
     var actualType = typeof item;
 
-    var specialValFn = specialValidations[expectedType];
+    var specialValFn = typeChecker[expectedType];
     if(specialValFn){
         isCorrectType = specialValFn(item);
     } else {
@@ -39,7 +51,29 @@ Validator.prototype.arg = function arg(item, expectedType, argName){
 };
 
 Validator.prototype.args = function args(obj, spec){
+    var isValid, expectedType, isCurrentKeyValid;
+    isValid = this.arg(obj, 'object') && this.arg(spec, 'object');
 
+    if(isValid){
+        for(var key in spec){
+            if(spec.hasOwnProperty(key)){
+                expectedType = spec[key];
+                if(typeof expectedType === 'object'){
+                    //either the parameter is optional or nested
+                }
+                // just normal validation
+                else {
+                    isCurrentKeyValid = this.arg(obj[key], expectedType, key);
+                }
+                // If one item fails, the whole thing fails
+                if(!isCurrentKeyValid){
+                    isValid = false;
+                }
+            }
+        }
+    }
+
+    return isValid;
 };
 
 
